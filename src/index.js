508c7b5cf1dab8f4df67bedbf1e74091e4caadc2
Kvttimus <glitch8077@gmail.com>
2025-03-06 01:56:46 +0000

    WORKING VERSION - Successfully imported discord.js, axios, and dotenv libraries

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 require('dotenv').config({path: '../.env'});
 
 const token = process.env.DISCORD_TOKEN;
+
+
+// Import required packages
+const { Client, GatewayIntentBits } = require('discord.js');  // allows bot to receive msgs, etc.
+const axios = require('axios');  // fetch tech articles/memes
