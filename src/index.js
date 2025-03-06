73162cf295f98ad1e65269d6214a56f474a548ad
Kvttimus <glitch8077@gmail.com>
2025-03-06 02:04:27 +0000

    WORKING VERSION - src/index.js - Can successfully log in as the bot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// Using CommonJS instead of ESM for simplicity
+
 // Load the environment variables using dotenv package
 require('dotenv').config({path: '../.env'});
-
-const token = process.env.DISCORD_TOKEN;
-
 
 // Import required packages
 const { Client, GatewayIntentBits } = require('discord.js');  // allows bot to receive msgs, etc.
 const axios = require('axios');  // fetch tech articles/memes
+
+// Get bot token from .env
+const token = process.env.DISCORD_TOKEN;
+
+
+// Create a new client instance
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+client.login(token);
