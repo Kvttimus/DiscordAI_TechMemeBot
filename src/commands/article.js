7f1cc5fe6eb698d9faa45cac5f8cfa849cf4b88d
kvttimus <glitch8077@gmail.com>
2025-03-16 21:52:24 +0000

    WORKING VERSION - Added outline for article cmd which sends tech artices + implemented a tech article scraper in src/utils/scrapeTechNews using hacker news API to scrape newest articles & store their links & titles

---
+++ src/commands/article.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'article', 
+    description: 'sends tech articles',
+    async execute(interaction) {
+        await interaction.reply("sent ARTICLEEEE");
+    }
+};
---
+++ src/utils/scrapeTechNews.js
@@ -0,0 +1,39 @@
+/*
+- Identifies & scrapes news articles from HackerNews
+*/
+
+const axios = require('axios');
+
+// limit: # of tech articles scraped
+async function fetchHackerNewsArticles ( limit = 5 ) {
+    try {
+        // get list of top story IDs
+        const topStoriesUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+        const { data: storyIds } = await axios.get(topStoriesUrl);
+
+        // get details of the top 'limit' stories
+        const articles = await Promise.all(
+            storyIds.slice(0, limit).map(async (id) => {
+                const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
+
+                const { data: story } = await axios.get(storyUrl);
+                console.log(`found title: ${story.title}`)  // DEBUG
+                console.log(`found url: ${story.url}`)  // DEBUG
+                
+                // the story?.title returns undefined/null instead of an error
+                return { title: story?.title, url: story.url };  
+            })
+        );
+
+        console.log("fetched articles!!");
+        return articles;
+    } catch (error) {
+        console.log("error fetching HackerNews articles", error);
+        return [];
+    }
+};
+
+// test function
+fetchHackerNewsArticles().then(console.log());
+
+module.exports = {fetchHackerNewsArticles};
